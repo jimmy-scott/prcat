861748cd85be87143cf5dfd6d47d6ad1d62c2bac
Jimmy Scott <jimmy@inet-solutions.be>
2012-10-05 18:08:57 +0200

    Add buffer code
    This can be used to pass data from the proxy to the tunnel part.

---
+++ src/buffer.h
@@ -0,0 +1,46 @@
+/*
+ * Copyright (C) 2012 Jimmy Scott #jimmy#inet-solutions#be#. Belgium.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *  1. Redistributions of source code must retain the above copyright notice,
+ *     this list of conditions and the following disclaimer.
+ *
+ *  2. Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *
+ *  3. The names of the authors may not be used to endorse or promote products
+ *     derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS "AS IS" AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+ * THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef _BUFFER_H_
+#define _BUFFER_H_
+
+#ifndef BUFFER_T_SIZE
+#define BUFFER_T_SIZE 4096
+#endif /* BUFFER_T_SIZE */
+
+typedef struct buffer_t {
+	char data[BUFFER_T_SIZE];
+	short s_len;	/* bytes stored */
+	short w_len;	/* bytes written */
+} buffer_t;
+
+void buffer_init(struct buffer_t *buffer);
+
+#endif /* _BUFFER_H_ */
